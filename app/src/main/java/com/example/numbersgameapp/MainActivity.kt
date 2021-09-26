28505bdb1d17b04250c6c153bd11bfc48e5d28f0
Safa <sfalaqi@gmail.com>
2021-09-26 17:20:38 +0300

    initial commit

--- app/src/main/java/com/example/numbersgameapp/MainActivity.kt
+++ app/src/main/java/com/example/numbersgameapp/MainActivity.kt
@@ -25,25 +25,27 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button)
         val editText = findViewById<EditText>(R.id.enterNumber)
         val myRv = findViewById<RecyclerView>(R.id.rvMain)
-        //guess limit
 
 
             button.setOnClickListener {
+                //first check if empty and if yes inform the user
                 if (editText.text.isEmpty()) {
                     Toast.makeText(this, "Please enter a number ", Toast.LENGTH_SHORT).show()
                     editText.getText().clear()
-                } else if(guess==0)
+                } else if(guess==0)  //if we have 0 guess
                     {
+                        //show the user a dialog if want to play again or not
                         showAlertDialog()
                         editText.getText().clear()
-                     } else if(random == Integer.parseInt(editText.text.toString())) {
+                     } //if user give a correct guess print in recycler view that it is correct
+                        else if(random == Integer.parseInt(editText.text.toString())) {
                         texts.add("You guessed ${editText.text.toString()}\n Correct Guess! You win.")
                         //then clear the Edit Text field.
                         editText.getText().clear()
                         myRv.adapter = RecyclerViewAdapter(texts)
                         myRv.layoutManager = LinearLayoutManager(this)
+                        //set guess to 0 so the dialog box appear and the user will choose to play or not
                         guess=0
-                        showAlertDialog()
                     } else {
                         texts.add("You guessed ${editText.text.toString()}\n You have $guess guesses left")
                         //then clear the Edit Text field.
@@ -55,6 +57,8 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+    //this function is used to show Alert Dialog for the user
     fun showAlertDialog()
     {
 
